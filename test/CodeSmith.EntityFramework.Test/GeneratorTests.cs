3f315ab8fdb5965e6126b80382f042181b452bf1
zhongkai1010 <zhongkai1010@163.com>
2015-11-29 09:27:36 +0800

    测试

---
+++ test/CodeSmith.EntityFramework.Test/GeneratorTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeSmith.EntityFramework;
+using NUnit.Framework;
+using SchemaExplorer;
+
+namespace CodeSmith.EntityFramework.Test
+{
+    [TestFixture()]
+    public class GeneratorTests
+    {
+        [Test()]
+        public void GeneratorTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GenerateTest()
+        {
+            Generator generator = new Generator();
+            IDbSchemaProvider dbSchemaProvider = new MySQLSchemaProvider();
+            generator.Generate(new SchemaSelector(dbSchemaProvider, "Server=127.0.0.1;Database=world;Uid=root;Pwd=123qwe!@#;"));
+            Assert.Fail();
+        }
+    }
+}
